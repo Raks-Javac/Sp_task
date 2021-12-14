03f58e2974d718d50da58df09eff88cb73c3b3dd
Raks-Javac <rufaikudus2014@gmail.com>
2021-12-14 21:28:15 +0100

    fix/main method fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_project_template/src/core/cache/gen_cache.dart';
 import 'package:flutter_project_template/src/locator.dart';
 import 'package:flutter_project_template/src/ui/main_platform.dart/main_mobile.dart';
-import 'package:flutter_project_template/src/ui/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-
-  // ignore: avoid_print
-  print(dotenv.env['API_URL']);
-
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   setupLocator();
